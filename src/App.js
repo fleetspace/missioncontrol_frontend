486c7436a0a9139ab65f458e29d176947c261b51
Louis des Landes <louis.deslandes@fleet.space>
2018-12-12 09:47:43 +1030

    Move app from missioncontrol repo

--- src/App.js
+++ src/App.js
@@ -1,28 +1,291 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+var _createClass = function () {
+  function defineProperties(target, props) {
+    for (var i = 0; i < props.length; i++) {
+      var descriptor = props[i];
+      descriptor.enumerable = descriptor.enumerable || false;
+      descriptor.configurable = true;
+      if ("value" in descriptor) descriptor.writable = true;
+      Object.defineProperty(target, descriptor.key, descriptor);
+    }
+  }
+  return function (Constructor, protoProps, staticProps) {
+    if (protoProps) defineProperties(Constructor.prototype, protoProps);
+    if (staticProps) defineProperties(Constructor, staticProps);
+    return Constructor;
+  };
+}();
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+function _classCallCheck(instance, Constructor) {
+  if (!(instance instanceof Constructor)) {
+    throw new TypeError("Cannot call a class as a function");
   }
 }
 
-export default App;
+function _possibleConstructorReturn(self, call) {
+  if (!self) {
+    throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
+  }
+  return call && (typeof call === "object" || typeof call === "function") ? call : self;
+}
+
+function _inherits(subClass, superClass) {
+  if (typeof superClass !== "function" && superClass !== null) {
+    throw new TypeError("Super expression must either be null or a function, not " + typeof superClass);
+  }
+  subClass.prototype = Object.create(superClass && superClass.prototype, {
+    constructor: {
+      value: subClass,
+      enumerable: false,
+      writable: true,
+      configurable: true
+    }
+  });
+  if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass;
+}
+var REST_API = '/api/v0/accesses/';
+
+// The root component
+var App = function App(props) {
+  return (
+    React.createElement('div', {
+      className: 'app'
+    },
+      React.createElement(ClockContainer, null),
+      React.createElement(UserListContainer, null)
+    )
+  )
+};
+
+// Clock
+var ClockContainer = function (_React$Component) {
+  _inherits(ClockContainer, _React$Component);
+
+  function ClockContainer(props) {
+    _classCallCheck(this, ClockContainer);
+    var _this = _possibleConstructorReturn(this, (ClockContainer.__proto__ || Object.getPrototypeOf(ClockContainer)).call(this,
+      props));
+    _this.state = {
+      utc: new Date().toISOString()
+    };
+    return _this;
+
+  }
+
+  _createClass(ClockContainer, [{
+    key: 'loadData',
+    value: function loadData() { }
+  }, {
+    key: 'componentWillMount',
+    value: function componentWillMount() { }
+  }, {
+    key: 'componentDidMount',
+    value: function componentDidMount() {
+
+      // Data is loaded async, therefore will be inserted after first DOM rendering
+      setInterval(() => {
+        this.setState({
+          curTime: new Date().toISOString()
+        })
+      }, 1000)
+    }
+  }, {
+    key: 'componentWillReceiveProps',
+    value: function componentWillReceiveProps(
+      nextProps) { }
+  }, {
+    key: 'shouldComponentUpdate',
+    value: function shouldComponentUpdate(
+      nextProps, nextState) {
+      return true;
+    }
+  }, {
+    key: 'componentWillUpdate',
+    value: function componentWillUpdate(
+      nextProps, nextState) { }
+  }, {
+    key: 'componentDidUpdate',
+    value: function componentDidUpdate(
+      prevProps, prevState) { }
+  }, {
+    key: 'componentWillUnmount',
+    value: function componentWillUnmount() { }
+  }, {
+    key: 'render',
+    value: function render() {
+      return React.createElement(ClockView, {
+        utc: this.state.curTime
+      });
+    }
+  }]);
+  return ClockContainer;
+}(React.Component);
+
+
+// Container
+var UserListContainer = function (_React$Component) {
+  _inherits(UserListContainer, _React$Component);
+
+  function UserListContainer(props) {
+    _classCallCheck(this, UserListContainer);
+    var _this = _possibleConstructorReturn(this, (UserListContainer.__proto__ || Object.getPrototypeOf(UserListContainer)).call(this,
+      props));
+    _this.state = {
+      accesses: [],
+    };
+    return _this;
+
+
+  }
+  _createClass(UserListContainer, [{
+    key: 'loadData',
+    value: function loadData() {
+      var _this2 = this;
+      axios.get(REST_API).then(function (response) {
+        //console.log(response.data);
+        _this2.setState({
+          accesses: response.data
+        });
+
+      });
+    }
+
+    // Life cycles hooks
+    // facebook.github.io/react/docs/component-specs.html
+  }, {
+    key: 'componentWillMount',
+    value: function componentWillMount() {
+      var tchart = TimelinesChart()(document.body)
+        .zScaleLabel('My Scale Units')
+        .zQualitative(true)
+        .timeFormat("%Y-%m-%dT%H:%M:%S.%LZ")
+        .useUtc(true)
+
+      this.setState({ chart: tchart })
+    }
+  }, {
+    key: 'componentDidMount',
+    value: function componentDidMount() {
+      // Data is loaded async, therefore will be inserted after first DOM rendering
+      setInterval(() => {
+        this.loadData();
+      }, 300000)
+      this.loadData();
+    }
+  }, {
+    key: 'componentWillReceiveProps',
+    value: function componentWillReceiveProps(
+      nextProps) { }
+  }, {
+    key: 'shouldComponentUpdate',
+    value: function shouldComponentUpdate(
+      nextProps, nextState) {
+      return true;
+    }
+  }, {
+    key: 'componentWillUpdate',
+    value: function componentWillUpdate(
+      nextProps, nextState) { }
+  }, {
+    key: 'componentDidUpdate',
+    value: function componentDidUpdate(
+      prevProps, prevState) { }
+  }, {
+    key: 'componentWillUnmount',
+    value: function componentWillUnmount() { }
+  }, {
+    key: 'render',
+    value: function render() {
+      return React.createElement(UserList, {
+        accesses: this.state.accesses,
+        chart: this.state.chart
+      });
+    }
+  }]);
+  return UserListContainer;
+}(React.Component);
+
+
+// Presentation
+var UserItem = function UserItem(access, index) {
+  return (
+    React.createElement('li', {
+      key: index
+    },
+      React.createElement('div', {
+        className: 'header'
+      },
+
+        React.createElement('div', {
+          className: 'name'
+        }, ' ', access.groundstation),
+
+        React.createElement('div', {
+          className: 'name'
+        }, ' ', access.satellite),
+
+        React.createElement('div', {
+          className: 'name'
+        }, ' ', access.start_time),
+
+        React.createElement('div', {
+          className: 'name'
+        }, ' ', access.end_time),
+
+        React.createElement('span', null, access.max_alt),
+      )
+    )
+  );
+};
+
+
+var ClockView = function ClockView(props) {
+  return (
+    React.createElement('h1', null, ' ', props.utc)
+  );
+};
+
+
+var UserList = function UserList(props) {
+
+  var data = []
+  var satellites = Array.prototype.map.call(props.accesses, function (p) { return p.satellite })
+  var by_satellites = new Map(Array.prototype.map.call(satellites, function (s) { return [s, []] }))
+  for (p of props.accesses) {
+    by_satellites.get(p.satellite).push(p)
+  }
+
+  var res = []
+  for (var [sat, sat_accesses] of by_satellites) {
+    var labels = []
+    var by_gs = new Map(Array.prototype.map.call(sat_accesses, function (s) { return [s.groundstation, []] }))
+    console.log(by_gs)
+    for (p of sat_accesses) {
+      console.log(p)
+      by_gs.get(p.groundstation).push(p)
+    }
+    for (var [gs, gs_accesses] of by_gs) {
+      var label = { "label": gs, "data": [] }
+      for (acc of gs_accesses) {
+        var _window = {
+          "timeRange": [acc.start_time, acc.end_time],
+          "val": gs
+        }
+        label.data.push(_window)
+      }
+      labels.push(label)
+    }
+    group = { "group": sat, "data": labels }
+    res.push(group)
+  }
+  props.chart.data(res);
+  return (
+    React.createElement('div', {
+      className: 'access-list'
+    })
+  );
+};
+
+
+// Render
+ReactDOM.render(
+  React.createElement(App, null), document.getElementById('root'));
