ba3736fe134fec463929a604db26479275e31225
Louis des Landes <louis.deslandes@fleet.space>
2018-12-13 10:12:02 +1030

    Add material ui.

--- src/passdisplay/AccessTable.js
+++ src/passdisplay/AccessTable.js
@@ -1,43 +1,65 @@
 import React, { Component } from 'react'
 import moment from 'moment'
+import { withStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
-class AccessTable extends Component {
+const styles = theme => ({
+    root: {
+        width: '100%',
+        marginTop: theme.spacing.unit * 3,
+        overflowX: 'auto',
+    },
+    table: {
+        minWidth: 700,
+    },
+});
 
-    render() {
-        const rows = this.props.accesses.map(access => {
-            const start_time = moment(access.start_time)
-            const end_time = moment(access.end_time)
+const AccessTable = (props) => {
+    const { classes } = props
 
-            return (
-                <tr key={access.id}>
-                    <td>{access.id}</td>
-                    <td>{access.groundstation}</td>
-                    <td>{access.satellite}</td>
-                    <td>{start_time.toISOString()}</td>
-                    <td>{end_time.toISOString()}</td>
-                    <td>{start_time.toISOString(true)}</td>
-                    <td>{end_time.toISOString(true)}</td>
-                    <td>{access.max_alt}</td>
-                </tr>
-            )
-        })
-        console.log(this.props.accesses)
+    const rows = props.accesses.map(access => {
+        const start_time = moment(access.start_time)
+        const end_time = moment(access.end_time)
+
         return (
-            <table>
-                <tr>
-                    <th>ID</th>
-                    <th>Ground Station</th>
-                    <th>Satellite</th>
-                    <th>UTC start</th>
-                    <th>UTC end</th>
-                    <th>Local start</th>
-                    <th>Local end</th>
-                    <th>Max altitude</th>
-                </tr>
-                {rows}
-            </table>
+            <TableRow key={access.id}>
+                <TableCell>{access.id}</TableCell>
+                <TableCell>{access.groundstation}</TableCell>
+                <TableCell>{access.satellite}</TableCell>
+                <TableCell>{start_time.toISOString()}</TableCell>
+                <TableCell>{end_time.toISOString()}</TableCell>
+                <TableCell>{start_time.toISOString(true)}</TableCell>
+                <TableCell>{end_time.toISOString(true)}</TableCell>
+                <TableCell>{access.max_alt}</TableCell>
+            </TableRow>
         )
-    }
+    })
+    return (
+        <Paper className={classes.root}>
+            <Table className={classes.table}>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>ID</TableCell>
+                        <TableCell>Ground Station</TableCell>
+                        <TableCell>Satellite</TableCell>
+                        <TableCell>UTC start</TableCell>
+                        <TableCell>UTC end</TableCell>
+                        <TableCell>Local start</TableCell>
+                        <TableCell>Local end</TableCell>
+                        <TableCell>Max altitude</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {rows}
+                </TableBody>
+            </Table>
+        </Paper>
+    )
 }
 
-export default AccessTable
+export default withStyles(styles)(AccessTable)
